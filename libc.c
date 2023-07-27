b370c72038896dfcaa6dcb3b4b1e5c2812d3f929
Zaraksh Rahman <zarakshR@pm.me>
2023-07-27 17:58:47 +0100

    Make inline assembly in syscall use registers only

--- libc.c
+++ libc.c
@@ -17,13 +17,13 @@ long syscall(long NR, long arg1, long arg2, long arg3, long arg4, long arg5, lon
         :
         [retval] "=r" (retval)
         :
-        [NR] "rm" (NR),
-        [arg1] "rm" (arg1),
-        [arg2] "rm" (arg2),
-        [arg3] "rm" (arg3),
-        [arg4] "rm" (arg4),
-        [arg5] "rm" (arg5),
-        [arg6] "rm" (arg6)
+        [NR] "r" (NR),
+        [arg1] "r" (arg1),
+        [arg2] "r" (arg2),
+        [arg3] "r" (arg3),
+        [arg4] "r" (arg4),
+        [arg5] "r" (arg5),
+        [arg6] "r" (arg6)
         : 
         "rax",
         "rdi",
